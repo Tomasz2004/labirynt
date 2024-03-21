5ddf02f35dc75708f6a1b099e1564dff07855c93
Tomasz2004 <01187580@pw.edu.pl>
2024-03-21 18:08:55 +0100

    Poprawa zliczania kolumn

--- labirynt.c
+++ labirynt.c
@@ -5,31 +5,30 @@
 int n;   // Liczba wierszy
 int m;   // Liczba kolumn
 
-void rozmiar(const char* filename, FILE *tym) {
-        FILE* file = fopen(filename, "r");
-        if (!file) {
-                printf("Nie można otworzyć pliku.\n");
-                exit(1);
-        }
-        char znak;
-        char s[2048];
-        int i = 0, j = 0; // Liczniki wierszy i kolumn
+void rozmiar(FILE* filename, FILE *tym) {
+    if (!filename) {
+        printf("Nie można otworzyć pliku.\n");
+        exit(1);
+    }
 
-        while ((znak = fgetc(file)) != '\n') {
-                j++; // Zliczanie kolumn w pierwszym wierszu
-        }
+    int znak;
+    char s[2048];
+    int i = 0; // Licznik wierszy
+    int j = 0; // Licznik kolumn
+    while ((znak = fgetc(filename)) == 'X' && znak != EOF) {
+        j++;
+    }
 
-        rewind(file); // Przewijamy do pocz¹tku pliku3
-        while (fgets(s, sizeof(s), file)) {
-            fputs(s,tym);
-            i++; // Inkrementujemy licznik wierszy
-        }
-        m = j;  // Liczba kolumn w L
-        n = i;  // Liczba wierszy w L
-        printf("Liczba kolumn %d i liczba wierszy %d\n",m,n);
+    rewind(filename); // Przewijamy do początku pliku
 
-        fclose(file);
-        fclose(tym);
+    while (fgets(s, sizeof(s), filename)) {
+        fputs(s, tym);
+        i++; // Inkrementujemy licznik wierszy
+    }
+
+    m = j; // Liczba kolumn w L
+    n = i; // Liczba wierszy w L
+    printf("Liczba kolumn %d i liczba wierszy %d\n", m, n);
 }
 void czytaj(FILE *file) {
     if (!file) {
@@ -41,20 +40,20 @@ void czytaj(FILE *file) {
         printf("%c", c);
     }
     printf("\n");
-    fclose(file);
 }
 
 int main() {
-    const char* filename = "maze.txt";
+    const char* filename = "smallmaze.txt";
+    FILE* file = fopen(filename, "r");
     FILE *lab = fopen("lab.txt", "w");
     if (!lab) {
         printf("Nie można utworzyć pliku\n");
         return 3;
     }
-    
-    rozmiar(filename, lab);
+
+    rozmiar(file, lab);
+    fclose(lab);
     lab = fopen("lab.txt", "r");
-    rewind(lab);
     if (!lab) {
         printf("Nie można otworzyć pliku do odczytu\n");
         return 4;
@@ -62,6 +61,7 @@ int main() {
     printf("Labirynt: \n");
     czytaj(lab);
     fclose(lab);
+    fclose(file);
     return 0;
 }
 
